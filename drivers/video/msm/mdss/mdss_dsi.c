24ad0aec51442f9e5552a770ea73178984fea468
Sultanxda <sultanxda@gmail.com>
2017-05-03 22:30:23 -0700

    msm: mdss: Don't constantly fire interrupts for DSI status check
    The DSI status check that uses the vsync GPIO results in an interrupt
    being fired every time a vsync event occurs, which results in several
    interrupts being fired and processed constantly for no reason.
    
    Instead of needlessly letting the vsync interrupt fire non-stop, force it
    to fire at most twice every STATUS_CHECK_INTERVAL_MS (5000 ms). This
    results in reduced CPU usage for this feature.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1588,13 +1588,9 @@ static int mdss_dsi_unblank(struct mdss_panel_data *pdata)
 	}
 
 	if ((pdata->panel_info.type == MIPI_CMD_PANEL) &&
-		mipi->vsync_enable && mipi->hw_vsync_mode) {
+		mipi->vsync_enable && mipi->hw_vsync_mode)
 		mdss_dsi_set_tear_on(ctrl_pdata);
-		if (mdss_dsi_is_te_based_esd(ctrl_pdata)){
-				if (mdss_dsi_is_te_based_esd(ctrl_pdata))
-					schedule_delayed_work(&(ctrl_pdata->techeck_work), msecs_to_jiffies(3000));
-			}
-	}
+
 	ctrl_pdata->ctrl_state |= CTRL_STATE_PANEL_INIT;
 
 error:
@@ -1661,14 +1657,8 @@ static int mdss_dsi_blank(struct mdss_panel_data *pdata, int power_state)
 	}
 
 	if ((pdata->panel_info.type == MIPI_CMD_PANEL) &&
-		mipi->vsync_enable && mipi->hw_vsync_mode) {
-		if (mdss_dsi_is_te_based_esd(ctrl_pdata)) {
-			if (mdss_dsi_is_te_based_esd(ctrl_pdata))
-				cancel_delayed_work_sync(&(ctrl_pdata->techeck_work));
-				atomic_dec(&ctrl_pdata->te_irq_ready);
-		}
+		mipi->vsync_enable && mipi->hw_vsync_mode)
 		mdss_dsi_set_tear_off(ctrl_pdata);
-	}
 
 	if (ctrl_pdata->ctrl_state & CTRL_STATE_PANEL_INIT) {
 		if (!pdata->panel_info.dynamic_switch_pending) {
@@ -3274,7 +3264,7 @@ static int mdss_dsi_ctrl_probe(struct platform_device *pdev)
 		INIT_DELAYED_WORK(&(ctrl_pdata->techeck_work), techeck_work_func );
 		rc = devm_request_irq(&pdev->dev,
 			gpio_to_irq(ctrl_pdata->disp_te_gpio),
-			hw_vsync_handler, IRQF_TRIGGER_FALLING,
+			hw_vsync_handler, IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
 			"VSYNC_GPIO", ctrl_pdata);
 		if (rc) {
 			pr_err("TE request_irq failed.\n");
