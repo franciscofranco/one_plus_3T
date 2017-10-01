e8fd128511893ecd67ec451aee2721275ec91200
Francisco Franco <franciscofranco.1990@gmail.com>
2017-10-01 16:39:24 +0000

    oneplus: add some error wakelock cleanups
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/power/oneplus_fastcg.c
+++ drivers/power/oneplus_fastcg.c
@@ -1019,6 +1019,8 @@ err_misc_register_failed:
 err_read_dt:
 	kfree(di);
 err_check_functionality_failed:
+	wake_lock_destroy(&di->fastchg_wake_lock);
+	wake_lock_destroy(&di->fastchg_update_fireware_lock);
 	pr_err("dash_probe fail\n");
 	return 0;
 }
@@ -1036,6 +1038,9 @@ static int dash_remove(struct i2c_client *client)
 		gpio_free(di->ap_clk);
 	if (gpio_is_valid(di->ap_data))
 		gpio_free(di->ap_data);
+
+	wake_lock_destroy(&di->fastchg_wake_lock);
+        wake_lock_destroy(&di->fastchg_update_fireware_lock);
 
 	return 0;
 }
