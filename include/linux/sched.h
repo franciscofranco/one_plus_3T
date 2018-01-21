602a60dfdf057479372a01305580a1c8829123df
Francisco Franco <franciscofranco.1990@gmail.com>
2018-01-21 03:20:17 +0000

    sched: fix merge derp
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1873,16 +1873,6 @@ static inline pid_t task_tgid_nr(struct task_struct *tsk)
 	return tsk->tgid;
 }
 
-static inline pid_t task_tgid_nr_ns(struct task_struct *tsk, struct pid_namespace *ns)
-{
-	return __task_pid_nr_ns(tsk, __PIDTYPE_TGID, ns);
-}
-
-static inline pid_t task_tgid_vnr(struct task_struct *tsk)
-{
-	return __task_pid_nr_ns(tsk, __PIDTYPE_TGID, NULL);
-}
-
 static inline int pid_alive(const struct task_struct *p);
 
 static inline pid_t task_pgrp_nr_ns(struct task_struct *tsk,
